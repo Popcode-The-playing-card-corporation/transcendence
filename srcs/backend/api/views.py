d31993df9379bcebd55ba0889a2c59e975345de3
cyraullie <cyril.goldenschue@gmail.com>
2026-04-20 16:05:24 +0200

    delete debug part

--- srcs/backend/api/views.py
+++ srcs/backend/api/views.py
@@ -5,14 +5,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 
-@api_view(["GET", "PUT", "PATCH"])
+@api_view(["PUT", "PATCH"])
 def update_user(request, id):
 
     user = get_object_or_404(User, id=id)
-
-    if request.method == "GET":
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
 
     serializer = UserSerializer(user, data=request.data, partial=True)
 
