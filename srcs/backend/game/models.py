15022ed2d3bfe60c20eb60c8c2dee9d5a8e68e3d
cyraullie <cyril.goldenschue@gmail.com>
2026-04-21 16:29:41 +0200

    create models to game DB

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from api.models import User
 
 # Create your models here.
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hosted_rooms')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.code}"
+
+class PlayerScore(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='scores')
+    score = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ['player', 'room']
+        
+class PlayerPresence(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='presences')
+
+    is_online = models.BooleanField(default=False)
+    is_in_game = models.BooleanField(default=False)
+
+    last_seen = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['player', 'room']
