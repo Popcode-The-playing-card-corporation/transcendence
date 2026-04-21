ed496502c341132bf3fe532ec5dd75545a79b980
cyraullie <cyril.goldenschue@gmail.com>
2026-04-21 16:29:23 +0200

    add JWT to auth and get a token

--- srcs/backend/api/models.py
+++ srcs/backend/api/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    username = models.CharField(max_length=255, unique=True, blank=True, null=True)
 
     email = models.EmailField(unique=True)  # 🔥 IMPORTANT
 
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]  # ❗ email doit être exclu
--- srcs/backend/api/views.py
+++ srcs/backend/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404
 from .models import User
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.permissions import AllowAny
 
 @api_view(["PUT", "PATCH"])
 def update_user(request, id):
@@ -20,6 +23,7 @@ def update_user(request, id):
 
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
@@ -32,19 +36,22 @@ def register(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def login(request):
     username = request.data.get("username")
     password = request.data.get("password")
 
-    user = get_object_or_404(User, username=username)
+    user = authenticate(username=username, password=password)
 
-    if user.check_password(password):
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+
         return Response({
-            "message": "Login success",
-            "user_id": user.id
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
         })
 
     return Response(
         {"error": "Invalid credentials"},
-        status=status.HTTP_401_UNAUTHORIZED
+        status=401
     )
