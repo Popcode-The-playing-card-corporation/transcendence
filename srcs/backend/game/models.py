739c54943df5898889f5047acf123b9ab78bcfea
cyraullie <cyril.goldenschue@gmail.com>
2026-04-22 14:31:38 +0200

    add field is_started on room table

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -7,7 +7,8 @@ class Room(models.Model):
     code = models.CharField(max_length=8, unique=True)
     host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hosted_rooms')
     created_at = models.DateTimeField(auto_now_add=True)
-
+    is_started = models.BooleanField(default=False)
+    
     def __str__(self):
         return f"{self.code}"
 
