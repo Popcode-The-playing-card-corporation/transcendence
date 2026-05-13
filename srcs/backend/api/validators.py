4a5c2ec3f893bffb4bac447271ff5cf7fe6ade14
cyraullie <cyril.goldenschue@gmail.com>
2026-05-13 10:19:21 +0200

    change error message in english

--- srcs/backend/api/validators.py
+++ srcs/backend/api/validators.py
@@ -2,26 +2,27 @@
 import re
 from django.core.exceptions import ValidationError
 
+
 class CustomPasswordValidator:
     def validate(self, password, user=None):
 
         if not re.search(r'[A-Z]', password):
             raise ValidationError(
-                "Le mot de passe doit contenir une majuscule."
+                "The password must contain at least one uppercase letter."
             )
 
         if not re.search(r'[a-z]', password):
             raise ValidationError(
-                "Le mot de passe doit contenir une minuscule."
+                "The password must contain at least one lowercase letter."
             )
 
         if not re.search(r'[\W_]', password):
             raise ValidationError(
-                "Le mot de passe doit contenir un caractère spécial."
+                "The password must contain at least one special character."
             )
 
     def get_help_text(self):
         return (
-            "Votre mot de passe doit contenir "
-            "une majuscule, une minuscule et un caractère spécial."
+            "Your password must contain at least one uppercase letter, "
+            "one lowercase letter, and one special character."
         )
