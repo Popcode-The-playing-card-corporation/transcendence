2190620e9b267dc4ae3e310b658a225a1b6974cc
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-05-18 14:16:27 +0200

    Ready for PR, testAccount fixed

--- srcs/frontend/src/utils/test_funcs/generateTestAccount.ts
+++ srcs/frontend/src/utils/test_funcs/generateTestAccount.ts
@@ -5,10 +5,11 @@ export function generateFakeAccount(): accountT {
   return {
     username: "Alex le mec qui critique les autres mais qui est pas mieux, genre vraiment un hypocrite ce mec",
     email: "atomasi42@proton.me",
-    password: "cacaboudin",
     avatar: avatar_test,
     date_joined: "04-02-2042",
     is_online: true,
     last_login: "28-04-2025",
+	id: 67, // Tu m'as menti Dana! tu n'as pas respecte le type smh :(
+	elo: 69,
   };
 }
