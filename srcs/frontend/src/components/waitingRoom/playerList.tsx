b58f3fd8d65b282aed94fa761c30d9c1e7ac2ed2
neoo-n <dana.vauthey@gmail.com>
2026-05-27 14:36:06 +0200

    anouar brokes everything

--- srcs/frontend/src/components/waitingRoom/playerList.tsx
+++ srcs/frontend/src/components/waitingRoom/playerList.tsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import generateFakePlayerList from "../../utils/test_funcs/generateFakePlayerList";
 import UsernameMiniProfileBtn from "../MiniProfile/UsernameMiniProfileBtn";
 
 export default function PlayerList() {
   const fakePlayers = generateFakePlayerList();
+  const [fakeBool, setFakeBool] = useState<boolean>(false);
 
   return (
     <div className="bordered">
       <table className="table-auto">
-        <thead className="text-xl">
+        <thead className="text-xl ">
           <tr>List of players</tr>
         </thead>
         <tbody>
-          <tr>
             {fakePlayers.map((player) => (
-              <UsernameMiniProfileBtn id={player.id} updatedFriends={} setUpdate={}/>
+              <tr>
+                <td >
+                  <UsernameMiniProfileBtn id={player.id} name={player.username} updatedFriends={fakeBool} setUpdate={setFakeBool} />
+                </td>
+              </tr>
             ))}
-          </tr>
         </tbody>
       </table>
     </div>
