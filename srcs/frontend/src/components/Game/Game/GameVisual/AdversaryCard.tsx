3651cd1622371a3501d45a24a93aa35a6d4ac63d
neoo-n <dana.vauthey@gmail.com>
2026-06-11 01:01:32 +0200

    its broken

--- srcs/frontend/src/components/Game/Game/GameVisual/AdversaryCard.tsx
+++ srcs/frontend/src/components/Game/Game/GameVisual/AdversaryCard.tsx
@@ -2,13 +2,12 @@ import { MeshPhongMaterial, TextureLoader } from "three";
 import type { Texture, TextureEventMap } from "three";
 
 type Props = {
-  angleY: number, 
-  angleZ: number, 
+  angle: number, 
   distance: number, 
   textureBack: Texture<HTMLImageElement, TextureEventMap> 
 }
 
-export default function AdversaryCard({angleY, angleZ, distance, textureBack} : Props){
+export default function AdversaryCard({angle,distance, textureBack} : Props){
   const materials = [
     new MeshPhongMaterial({color: 0xffffff}),
     new MeshPhongMaterial({color: 0xffffff}),
@@ -20,8 +19,8 @@ export default function AdversaryCard({angleY, angleZ, distance, textureBack} : 
 
   return (
     <mesh
-      rotation={[Math.PI / 2, angleY, angleZ]}
-      position={[0, 0, 0]}
+      rotation={[Math.PI / 2, angle, 0]}
+      position={[Math.sin(angle) * distance, -Math.cos(angle) * distance, 0]}
       material={materials}
     >
       <boxGeometry args={[1, 1.4, 0.01]}/>
--- srcs/frontend/src/components/Game/Game/GameVisual/AdversaryHand.tsx
+++ srcs/frontend/src/components/Game/Game/GameVisual/AdversaryHand.tsx
@@ -11,20 +11,20 @@ type Props = {
 
 export default function AdversaryHand({cardHand, textureBack, total} : Props) {
   const { viewport } = useThree();
-  const angleBetween = 360 / cardHand.nbCards * Math.PI / 180;
+  const angleBetween = 0.03;
   const distanceBetweenCard = 0.3;
-  const distanceCenter = viewport.height / 2 + 0.3;
-  const angleCenter = (360* (cardHand.position + 1) /total) * (Math.PI / 180);
+  const distanceStart = viewport.height / 2 + 0.3;
+  const angleStart = (360 * (cardHand.position + 1) / total) * (Math.PI / 180) - cardHand.nbCards * angleBetween / 2;
   const allAngle : number[] = [];
   for (let i = 0; i < cardHand.nbCards; i++)
-    allAngle.push(angleCenter + i * angleBetween);
+    allAngle.push(angleStart + i * angleBetween);
 
   return (
     <mesh
     >
       {allAngle.map((angle) => {
         return (
-          <AdversaryCard distance={0} angleY={angle} angleZ={angleBetween} textureBack={textureBack}/>
+          <AdversaryCard distance={distanceStart + allAngle.indexOf(angle) * distanceBetweenCard} angle={angle} textureBack={textureBack}/>
         );
       })}
     </mesh>
