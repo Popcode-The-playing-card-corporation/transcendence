86163cfd255a4ee7c6e5c2492909c8a1688d1c25
Crealex <Alexandre.tomasi80@gmail.com>
2026-06-18 16:26:07 +0200

    created limiters param in Waiting room

--- srcs/frontend/src/components/Game/waitingRoom/ParameterRoom.tsx
+++ srcs/frontend/src/components/Game/waitingRoom/ParameterRoom.tsx
@@ -1,13 +1,14 @@
 import AddingBot from "./AddingBot";
+import Limiters from "./Limiters";
 import PrivatePublicSlider from "./PrivateFriendPublicSlider";
 import RoomSize from "./RoomSize";
 
 type Props = {
-	roomCode: string;
-	updateSettings: () => void;
-}
+  roomCode: string;
+  updateSettings: () => void;
+};
 
-export default function ParameterRoom({roomCode, updateSettings}  : Props) {
+export default function ParameterRoom({ roomCode, updateSettings }: Props) {
   return (
     <div className="bordered grid grid-cols-3 gap-4">
       <div className="col-span-3 grid grid-cols-3 pt-6">
@@ -27,17 +28,26 @@ export default function ParameterRoom({roomCode, updateSettings}  : Props) {
         </div>
       </div>
       <div className="col-span-3 grid grid-cols-3 border-t border-(--hover-color) pt-6">
+        <div>
+          <p>Choose the limiter : </p>
+        </div>
+        <div className="col-span-2 mb-6">
+          <Limiters />
+        </div>
+      </div>
+      <div className="col-span-3 grid grid-cols-3 border-t border-(--hover-color) pt-6">
         <div className="">
           <p>Choose the level and the number of bots you want : </p>
         </div>
         <div className="col-span-2 mb-6">
-          <AddingBot roomCode={roomCode} updateSettings={updateSettings}/>
+          <AddingBot roomCode={roomCode} updateSettings={updateSettings} />
         </div>
         <div className="flex items-center justify-center col-span-3 border-t border-(--hover-color) pt-6">
-          <button className="btn" onClick={updateSettings}>Confirm</button>
+          <button className="btn" onClick={updateSettings}>
+            Confirm
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
