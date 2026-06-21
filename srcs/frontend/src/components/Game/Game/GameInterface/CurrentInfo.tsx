1affb2f9f0d9dad4a81d2628c8cf7dc25cddf00b
creAlex <Alexandre.tomasi80@gmail.com>
2026-06-21 14:15:26 +0200

    improve current info

--- srcs/frontend/src/components/Game/Game/GameInterface/CurrentInfo.tsx
+++ srcs/frontend/src/components/Game/Game/GameInterface/CurrentInfo.tsx
@@ -1,30 +1,27 @@
-import { TbClubsFilled } from "react-icons/tb"
-import { TiHeartFullOutline } from "react-icons/ti"
+import { TbClubsFilled } from "react-icons/tb";
+import { TiHeartFullOutline } from "react-icons/ti";
 import { BsFillSuitDiamondFill, BsFillSuitSpadeFill } from "react-icons/bs";
 import { FaQuestion } from "react-icons/fa";
 
-
-	function DisplayTrumpLogo({trump}: {trump:string}) {
-		if (trump === "club") {
-			return <TbClubsFilled />
-		}
-		else if (trump === "heart")
-			return <TiHeartFullOutline />
-		else if (trump === "diamond")
-			return <BsFillSuitDiamondFill />
-		else if (trump === "spade")
-			return <BsFillSuitSpadeFill />
-		else
-			return <FaQuestion />
-	}
+function DisplayTrumpLogo({ trump }: { trump: string }) {
+  if (trump === "club") {
+    return <TbClubsFilled />;
+  } else if (trump === "heart") return <TiHeartFullOutline />;
+  else if (trump === "diamond") return <BsFillSuitDiamondFill />;
+  else if (trump === "spade") return <BsFillSuitSpadeFill />;
+  else return <FaQuestion />;
+}
 
 export default function CurrentInfo() {
-	const trump = "heart"
+  const trump = "club";
+  const currentPlayer = "francis lalanne";
 
-	return (
-		<div className="border-t mt-4 w-full">
-			<p className="flex items-center">Atout: <DisplayTrumpLogo trump={trump}/> </p>
-			<p>Your turn: 4</p>
-		</div>
-	)
+  return (
+    <div className="border-y border-(--hover-color) mt-2 py-2 w-full flex flex-col items-center">
+      <p className="flex items-center gap-1">
+        Atout: <DisplayTrumpLogo trump={trump} />{" "}
+      </p>
+      <p><em>{currentPlayer}</em>  is playing</p>
+    </div>
+  );
 }
