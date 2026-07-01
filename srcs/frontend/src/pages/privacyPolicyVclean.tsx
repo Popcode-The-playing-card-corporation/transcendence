008ad3ecc94e0d40a2a9cd21d4ab0e89a28011be
creAlex <Alexandre.tomasi80@gmail.com>
2026-07-01 17:00:09 +0200

    continue privacy policies(want to die)

---
+++ srcs/frontend/src/pages/privacyPolicyVclean.tsx
@@ -0,0 +1,207 @@
+export default function privacyPolicy() {
+  return (
+    <div className="page-content mt-17">
+      <h1>Privacy Policy</h1>
+      <div>
+        <p>Last updated June 01 2026</p>
+      </div>
+      <div className="intro">
+        <p>
+          {" "}
+          This Privacy Notice for Popcode Inc ( 'we', 'us', or 'our' ),
+          describes how and why we might access, collect, store, use, and/or
+          share ( 'process' ) your personal information when you use our
+          services ( 'Services' ), including when you:
+        </p>
+        <ul>
+          <li>
+            Visit our website at{" "}
+            <a href="http://www.popcards.com" target="_blank">
+              http://www.popcards.com
+            </a>{" "}
+            or any website of ours that links to this Privacy Notice
+          </li>
+          <li>
+            Use Popcards. A online platform to play the Swiss card game "La
+            Misere" with friends.
+          </li>
+          <li>
+            Engage with us in other related ways, including any marketing or
+            events
+          </li>
+        </ul>
+        <p>
+          <strong>Questions or concerns?</strong>Reading this Privacy Notice
+          will help you understand your privacy rights and choices. We are
+          responsible for making decisions about how your personal information
+          is processed. If you do not agree with our policies and practices,
+          please do not use our Services. If you still have any questions or
+          concerns, please contact us at{" "}
+          <a href="mailto:popcorn.coders@gmail.com">popcorn.coders@gmail.com</a>
+          .
+        </p>
+      </div>
+      <div className="pp-summary">
+        <p>
+          This summary provides key points from our Privacy Notice, but you can
+          find out more details about any of these topics by clicking the link
+          following each key point or by using our table of contents below to
+          find the section you are looking for.
+          <strong>What personal information do we process?</strong> When you
+          visit, use, or navigate our Services, we may process personal
+          information depending on how you interact with us and the Services,
+          the choices you make, and the products and features you use. Learn
+          more about personal information you disclose to us
+        </p>
+        <p>
+          <strong>Do we process any sensitive personal information?</strong>{" "}
+          Some of the information may be considered 'special' or 'sensitive' in
+          certain jurisdictions, for example your racial or ethnic origins,
+          sexual orientation, and religious beliefs. We do not process sensitive
+          personal information.{" "}
+          <strong>Do we collect any information from third parties?</strong> We
+          do not collect any information from third parties.{" "}
+          <strong>How do we process your information?</strong> We process your
+          information to provide, improve, and administer our Services,
+          communicate with you, for security and fraud prevention, and to comply
+          with law. We may also process your information for other purposes with
+          your consent. We process your information only when we have a valid
+          legal reason to do so. Learn more about how we process your
+          information <br />{" "}
+          <strong>
+            {" "}
+            In what situations and with whichparties do we share personal
+            information?
+          </strong>{" "}
+          <br />
+          We may share information in specific situations and with specificthird
+          parties. Learn more aboutwhen and with whom we share your personal
+          information.
+        </p>
+        <p>
+          {" "}
+          <strong>How do we keep your information safe?</strong> We have
+          adequateorganisationaland technical processes and procedures in place
+          to protect your personal information. However, no electronic
+          transmission over the internet or information storage technology can
+          be guaranteed to be 100% secure, so we cannot promise or guarantee
+          that hackers, cybercriminals, or otherunauthorisedthird parties will
+          not be able to defeat our security and improperly collect, access,
+          steal, or modify your information. Learn more about{" "}
+          <a href="#PrivacyRight">how we keep your information safe.</a>
+        </p>
+        <p>
+          <strong>What are your rights?</strong> Depending on where you are
+          located geographically, the applicable privacy law may mean you have
+          certain rights regarding your personal information. Learn more about{" "}
+          <a href="#privacyRights">your privacy rights.</a>
+        </p>
+        <p>
+          <strong>How do you exercise your rights?</strong> The easiest way to
+          exercise your rights is by visiting{" "}
+          <a href="mailto:popcorn.coders@gmail.com">popcorn.coders@gmail.com</a>
+          , or by contacting us. We will consider and act upon any request in
+          accordance with applicable data protection laws.
+        </p>
+        <p>
+          Want to learn more about what we do with any information we collect?{" "}
+          <a href="#informationCollected">Review the Privacy Notice in full</a>.
+        </p>
+      </div>
+      <div>
+        <ol>
+          <li>
+            <a href="#">WHAT INFORMATION DO WE COLLECT?</a>
+          </li>
+          <li>
+            <a href="#">HOW DO WE PROCESS YOUR INFORMATION?</a>
+          </li>
+          <li>
+            <a href="#">
+              WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL
+              INFORMATION?
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+            </a>
+          </li>
+          <li>
+            <a href="#">DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</a>
+          </li>
+          <li>
+            <a href="#">HOW DO WE HANDLE YOUR SOCIAL LOGINS</a>
+          </li>
+          <li>
+            <a href="#">HOW LONG DO WE KEEP YOUR INFORMATION</a>
+          </li>
+          <li>
+            <a href="#">HOW DO WE KEEP YOUR INFORMATION SAFE</a>
+          </li>
+          <li>
+            <a href="#">WHAT ARE YOUR PRIVACY RIGHTS</a>
+          </li>
+          <li>
+            <a href="#">CONTROLS FOR DO-NOT-TRACK FEATURES</a>
+          </li>
+          <li>
+            <a href="#">DO UNITED STATES RESIDENTS HAVE SPECIFIC PRIVACY</a>
+          </li>
+          <li>
+            <a href="#">DO WE MAKE UPDATES TO THIS NOTICE</a>
+          </li>
+          <li>
+            <a href="#">HOW CAN YOU CONTACT US ABOUT THIS NOTICE</a>
+          </li>
+          <li>
+            <a href="#">
+              HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM
+              YOU?
+            </a>
+          </li>
+        </ol>
+      </div>
+      <div>
+        <h1>1.WHAT INFORMATION DO WE COLLECT?</h1>
+        <h3>Personnal information you disclose us</h3>
+        <p>
+          <strong>In Short:</strong> We collect personal information that you
+          provide to us.
+          <br /> <br />
+          We collect personal information that you voluntarily provide to us
+          when you register on the Services, express an interest in obtaining
+          information about us or our products and Services, when you
+          participate in activities on the Services, or otherwise when you
+          contact us.
+          <br /> <br />
+          Personal Information Provided by You. The personal information that we
+          collect depends on the context of your interactions with us and the
+          Services, the choices you make, and the products and features you use.
+          The personal information we collect may include the following:
+        </p>
+        <ul>
+          <li>email addresses</li>
+          <li>usernames</li>
+          <li>passwords</li>
+          <li>contact or authentication data</li>
+        </ul>
+        <p>
+          <strong>Sensitive Information.</strong> We do not process sensitive
+          information.
+        </p>
+        <p>
+          Social Media Login Data. We may provide you with the option to
+          register with us using your existing social media account details,
+          like your Facebook, X, or other social media account. If you choose to
+          register in this way, we will collect certain profile information
+          about you from the social media provider, as described in the section
+          called <a href="#socialLogin">HOW DO WE HANDLE YOUR SOCIAL LOGINS?</a> below.
+        </p>
+		  <p>All personal information that you provide to us must be true, complete, and accurate, and you must notify us of any changes to such personal information.</p>
+		  <h4>Google API</h4>
+		  <p>Our use of information received from Google APIs will adhere to <a href="https://developers.google.com/terms/api-services-user-data-policy" target="_blank">Google API Services User Data Policy</a>, including the <a href="https://developers.google.com/terms/api-services-user-data-policy#limited-use" target="_blank">Limited Use requirements</a>.</p>
+      </div>
+    </div>
+  );
+}
