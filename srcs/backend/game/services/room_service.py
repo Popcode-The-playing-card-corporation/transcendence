0603579fd67e0b0682a9f40eb76cda43ead47757
Kilian Tintim Goncalves <ptigars2004@gmail.com>
2026-07-06 11:01:14 +0200

    improve readability

--- srcs/backend/game/services/room_service.py
+++ srcs/backend/game/services/room_service.py
@@ -53,14 +53,11 @@ class RoomService:
             lambda: User.objects.get(id=user.id).presence_game)()
 
         await sync_to_async(
-            User.objects.filter(id=user.id).update
-		)(
-            presence_game=F("presence_game") - 1
-        )
+            User.objects.filter(id=user.id).update)(presence_game=F("presence_game") - 1)
         
         await sync_to_async(
-			PlayerPresence.objects.filter(player_id=user.id).update
-		)(is_online=False)
+				PlayerPresence.objects.filter(player_id=user.id).update
+				)(is_online=False)
         
         if (old_presence == 1): 
             room = await get_room_with_host(code)
