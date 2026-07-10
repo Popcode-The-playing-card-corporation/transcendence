66d4d8ec4f7003301f8b387b755fcb17062c8716
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-07-10 14:12:14 +0200

    hotfix of request list

--- srcs/frontend/src/OAuth/GoogleCallback.tsx
+++ srcs/frontend/src/OAuth/GoogleCallback.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import host from "../api/http/host";
 import { useNotif } from "../components/hooks/useNotif";
 import { useAuth } from "../components/hooks/useAuth";
+import { friendArray, getFriends } from "../api/http/friend";
+import type { friendT, requestT } from "../utils/type/friendType";
 
 export function GoogleCallback() {
   const navigate = useNavigate();
@@ -11,6 +13,40 @@ export function GoogleCallback() {
   const location = useLocation();
   const notif = useNotif();
   const hasRun = useRef(false);
+
+   function getRequests(friend_list: friendT[]): {
+	friends: friendT[];
+	requests: requestT[];
+	} {
+	const friends: friendT[] = [];
+	const requests: requestT[] = [];
+  
+	for (const friend of friend_list) {
+		if (friend.can_accept) {
+		requests.push({ id: friend.id, username: friend.user.username });
+		} else {
+		friends.push(friend);
+		}
+	}
+	return { friends: friends, requests: requests };
+  }
+
+  async function loginSuccess() {
+
+	const friendlist = await getFriends();
+	if ("code" in friendlist) {
+		auth.setHasFriendRequest(false);
+		return ;
+	}
+	const arr = friendArray(friendlist);
+	const filter = getRequests(arr);
+	if (filter.requests.length > 0) {
+		auth.setHasFriendRequest(true);
+	} else {
+		auth.setHasFriendRequest(false);
+	}
+
+  }
 
   useEffect(() => {
 	if (hasRun.current) return;
@@ -35,7 +71,8 @@ export function GoogleCallback() {
 		auth.setPass(res.data.has_pass);
 		auth.setLoggedIn(true);
 		const redirect = sessionStorage.getItem("login_redirect") || "/";
-		sessionStorage.removeItem("login_redirect");		
+		sessionStorage.removeItem("login_redirect");
+		loginSuccess();		
 		navigate(redirect);
       } catch {
 		notif?.showNotif("Login Error", "OAuth Login failed please try again.", 5000)
