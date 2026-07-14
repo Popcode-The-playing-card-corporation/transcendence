f0ef70063281e1d91539125cfba684970fa4fb3e
neoo-n <dana.vauthey@gmail.com>
2026-07-14 16:18:32 +0200

    improve border color and width for interface chat and info

--- srcs/frontend/src/components/Game/Game/GameInterface/Chat.tsx
+++ srcs/frontend/src/components/Game/Game/GameInterface/Chat.tsx
@@ -1,6 +1,6 @@
 import { IoSend } from "react-icons/io5";
 import ChatText from "./ChatText"
-import { useEffect, useRef, useState, type SetStateAction, type KeyboardEvent} from "react";
+import { useEffect, useRef, useState, type SetStateAction, type KeyboardEvent } from "react";
 import { useGame } from "../../context/GameContext";
 
 type Props = {
@@ -9,7 +9,7 @@ type Props = {
   setCount: React.Dispatch<SetStateAction<number>>
 }
 
-export default function Chat({setNewMessage, isAlreadyOpen, setCount}: Props) {
+export default function Chat({ setNewMessage, isAlreadyOpen, setCount }: Props) {
   const game = useGame();
   const messages = game.state.messages;
   const messageEndRef = useRef<HTMLDivElement>(null);
@@ -21,46 +21,45 @@ export default function Chat({setNewMessage, isAlreadyOpen, setCount}: Props) {
 
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === "Enter")
-    	buttonRef.current?.click();
+      buttonRef.current?.click();
   };
 
   useEffect(() => {
     if (messageEndRef.current)
       messageEndRef.current.scrollTop = messageEndRef.current.scrollHeight;
-	async function setterMessage() { 
-	  if (!isAlreadyOpen)
-      	setNewMessage(true);
-	  else
+    async function setterMessage() {
+      if (!isAlreadyOpen)
+        setNewMessage(true);
+      else
         setCount(game.state.messages.length);
-	}
-	setterMessage();
+    }
+    setterMessage();
   }, [messages, setNewMessage, isAlreadyOpen])
 
   if (!messages)
     return (null);
 
   function handleSend() {
-	if (message.trim().length !== 0) {
-		game.sendMessage("chat_message", message);
-		setMessage("");
-	}
+    if (message.trim().length !== 0) {
+      game.sendMessage("chat_message", message);
+      setMessage("");
+    }
   }
 
   return (
     <div
-      className="dropdown-scroll space-y-2"
+      className="dropdown-scroll space-y-2 "
       ref={messageEndRef}
     >
       {messages.map((message) => {
         return (
           <>
-            <ChatText info={message} key={messages.indexOf(message)}/>
+            <ChatText info={message} key={messages.indexOf(message)} />
           </>
         );
       })}
-      <div ref={messageEndRef}></div>
-      <div className="join w-full sticky -bottom-5 pb-4 bg-base-100 " onKeyDown={handleKeyDown}>
-        <input type="text" placeholder="Type here" className="input join-item" value={message} onChange={messageChange}/>
+      <div ref={messageEndRef} className="join w-full sticky -bottom-5 pb-4 bg-base-100 " onKeyDown={handleKeyDown}>
+        <input type="text" placeholder="Type here" className="input join-item" value={message} onChange={messageChange} />
         <button ref={buttonRef} className="btn join-item" onClick={handleSend} >
           <IoSend />
         </button>
