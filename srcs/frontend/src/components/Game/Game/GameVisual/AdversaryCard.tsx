a8d139f11a8b82819d113eda6df3cceaf16f6fdb
neoo-n <dana.vauthey@gmail.com>
2026-07-14 11:46:31 +0200

    show cards usestate

--- srcs/frontend/src/components/Game/Game/GameVisual/AdversaryCard.tsx
+++ srcs/frontend/src/components/Game/Game/GameVisual/AdversaryCard.tsx
@@ -1,10 +1,10 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef, type SetStateAction } from "react";
-import { MeshPhongMaterial} from "three";
+import { useRef } from "react";
+import { MeshPhongMaterial } from "three";
 import type { Mesh, Texture, TextureEventMap } from "three";
+import { useGame } from "../../context/GameContext";
 
 type Props = {
-  setShow: React.Dispatch<SetStateAction<boolean>>,
   angle: number,
   littleRadius: number,
   front: Texture<HTMLImageElement, TextureEventMap>,
@@ -16,51 +16,50 @@ type Props = {
   resetState: () => void,
 }
 
-export default function AdversaryCard({setShow, angle, littleRadius, front, back, positionCard, totalPlayer, posPlayedCard, animate, resetState} : Props){
+export default function AdversaryCard({ angle, littleRadius, front, back, positionCard, totalPlayer, posPlayedCard, animate, resetState }: Props) {
   const materials = [
-    new MeshPhongMaterial({color: 0xffffff}),
-    new MeshPhongMaterial({color: 0xffffff}),
-    new MeshPhongMaterial({color: 0xffffff}),
-    new MeshPhongMaterial({color: 0xffffff}),
-	new MeshPhongMaterial({map: front}),
-    new MeshPhongMaterial({map: back})
+    new MeshPhongMaterial({ color: 0xffffff }),
+    new MeshPhongMaterial({ color: 0xffffff }),
+    new MeshPhongMaterial({ color: 0xffffff }),
+    new MeshPhongMaterial({ color: 0xffffff }),
+    new MeshPhongMaterial({ map: front }),
+    new MeshPhongMaterial({ map: back })
   ];
+  const { setShow } = useGame();
 
   const cardRef = useRef<Mesh>(null!);
 
   const factor = 0.15 * ((7 - totalPlayer));
   const pf = [0, posPlayedCard, 0];
-  
-  
+
+
   useFrame(() => {
-    if (animate)
-    {
+    if (animate) {
       const delta = [pf[0] - cardRef.current.position.x, pf[1] - cardRef.current.position.y, pf[2] - cardRef.current.position.z];
-      if (delta[0] <= 0.1 && delta[1] <= 0.1 && delta[2] <= 0.1)
-      {
+      if (delta[0] <= 0.1 && delta[1] <= 0.1 && delta[2] <= 0.1) {
         resetState();
-		setShow(true);
+        setShow(true);
       }
-    const factorDelta = 1 / 10;
-    cardRef.current.rotation.x = 0;
-    cardRef.current.rotation.z = 0;
-    cardRef.current.position.x += delta[0] * factorDelta;
-    cardRef.current.position.y += delta[1] * factorDelta;
-    cardRef.current.position.z += delta[2] * factorDelta;
-  }
-});
+      const factorDelta = 1 / 10;
+      cardRef.current.rotation.x = 0;
+      cardRef.current.rotation.z = 0;
+      cardRef.current.position.x += delta[0] * factorDelta;
+      cardRef.current.position.y += delta[1] * factorDelta;
+      cardRef.current.position.z += delta[2] * factorDelta;
+    }
+  });
 
   return (
     <>
-    <mesh
-      rotation={[Math.PI / 2, 0, angle]}
-      position={[-Math.sin(angle) * littleRadius,- (littleRadius + factor) + 0.01 * positionCard, Math.cos(angle) * (littleRadius  / 3)]}
-      scale={0.4}
-      material={materials}
-      ref={cardRef}
+      <mesh
+        rotation={[Math.PI / 2, 0, angle]}
+        position={[-Math.sin(angle) * littleRadius, - (littleRadius + factor) + 0.01 * positionCard, Math.cos(angle) * (littleRadius / 3)]}
+        scale={0.4}
+        material={materials}
+        ref={cardRef}
       >
-      <boxGeometry args={[1, 1.4, 0.001]}/>
-    </mesh>
+        <boxGeometry args={[1, 1.4, 0.001]} />
+      </mesh>
     </>
   );
 }
