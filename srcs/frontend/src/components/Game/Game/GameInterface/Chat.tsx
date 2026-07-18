859cb787a42e956c8a8b6ff284113d11f78d7fb3
neoo-n <dana.vauthey@gmail.com>
2026-07-18 14:16:34 +0200

    chat

--- srcs/frontend/src/components/Game/Game/GameInterface/Chat.tsx
+++ srcs/frontend/src/components/Game/Game/GameInterface/Chat.tsx
@@ -25,8 +25,6 @@ export default function Chat({ setNewMessage, isAlreadyOpen, setCount }: Props) 
   };
 
   useEffect(() => {
-    if (messageEndRef.current)
-      messageEndRef.current.scrollTop = messageEndRef.current.scrollHeight;
     async function setterMessage() {
       if (!isAlreadyOpen)
         setNewMessage(true);
@@ -45,6 +43,9 @@ export default function Chat({ setNewMessage, isAlreadyOpen, setCount }: Props) 
       setMessage("");
     }
   }
+
+  if (messageEndRef.current)
+    messageEndRef.current.scrollTop = messageEndRef.current.scrollHeight;
 
   return (
     <div
