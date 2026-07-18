242f6950b2754d94cfbc270f40b0ff97f11dce01
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-07-18 17:53:44 +0200

    removed console debug

--- srcs/frontend/src/components/Game/createOrJoin/FilterGames.tsx
+++ srcs/frontend/src/components/Game/createOrJoin/FilterGames.tsx
@@ -33,7 +33,6 @@ export default function FilterGame({
       },
     );
 
-	console.debug(filteredMaxPlayerGames)
 
     const finalFilteredGames = filteredMaxPlayerGames.filter(
       (game: availableGameT) => {
